ce29bf4c69ad8e6b8a6adbed4be2ab9531c64ed9
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2017-05-03 00:05:39 +0700

    mnb

--- ESP8266APSocketServer/XamarinAppClient/XamarinAppClient/MainPage.xaml.cs
+++ ESP8266APSocketServer/XamarinAppClient/XamarinAppClient/MainPage.xaml.cs
@@ -1,35 +1,30 @@
-﻿using SocketLite.Services;
+﻿using Sockets.Plugin;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace XamarinAppClient
 {
     public partial class MainPage : ContentPage
     {
+        TcpSocketClient client;
+
         public MainPage()
         {
             InitializeComponent();
-            
         }
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            var tcpClient = new TcpSocketClient();
-            await tcpClient.ConnectAsync("ws://192.168.1.69", 81);
-
-            var helloWorld = "ledoff";
-
-            var bytes = Encoding.UTF8.GetBytes(helloWorld);
-            await tcpClient.WriteStream.WriteAsync(bytes, 0, bytes.Length);
-            var stream = tcpClient.ReadStream;
-            var response = Convert.ToString(stream);
-            label.Text = response;
-            tcpClient.Disconnect();
+            client = new TcpSocketClient();
+            try
+            {
+                await client.ConnectAsync("ws://192.168.1.69:81", "tcp");
+                client.WriteStream.WriteByte(Convert.ToByte("ledon"));
+                await client.WriteStream.FlushAsync();
+                await client.DisconnectAsync();
+            }
+            catch { }
         }
     }
 }
